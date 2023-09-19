0322411df07f2f1da5fa7fcabfb7cd8f6bda0992
gonzaloconcep <lasconchasgonzalo@gmail.com>
2023-09-19 17:50:21 +0200

    Feat: metricsWave in frontend WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), partytown({
+    config: {
+       // Agrega "window.trackMetrics" como función reenviada.
+       forward: ["window.trackMetrics"],
+    },
+  }),],
 });
